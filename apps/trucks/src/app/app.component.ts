e763392f3830f7be58e38a377145bd3ee8b1e703
eugene.sky <skytech4dotnet@gmail.com>
2022-05-21 19:00:13 -0500

    initial commit

---
+++ apps/trucks/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'skydev-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'trucks';
+}
