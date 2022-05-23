0aed89671162fa22884ecfb8c4eb5589dcbd3f81
eugene.sky <skytech4dotnet@gmail.com>
2022-05-23 16:50:53 -0500

    npx nx g component trucks --project=ui --export

---
+++ libs/ui/src/lib/trucks/trucks.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'skydev-trucks',
+  templateUrl: './trucks.component.html',
+  styleUrls: ['./trucks.component.scss'],
+})
+export class TrucksComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
